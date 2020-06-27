80b2850c4e160cf7167c1286df70e212d01f6aeb
VasylHafych <vasilgafich@gmail.com>
2020-06-27 18:52:09 +0200

    initial commit

---
+++ src/submit-job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+
+#SBATCH -o ./job.out.%j
+#SBATCH -e ./job.err.%j
+#SBATCH -D ./
+#SBATCH -J julia_dist
+
+# Note: Full node and shared node are mutually exclusive! Uncomment either one or the other!
+
+### Full node configuration:
+##SBATCH --nodes=1
+##SBATCH --tasks-per-node=1
+##SBATCH --cpus-per-task=32  # 32 on DRACO, use 40 on COBRA
+##SBATCH --partition=general  # necessary on DRACO
+
+### Shared node configuration:
+#SBATCH --partition=express
+#SBATCH --ntasks=20
+#SBATCH --cpus-per-task=6
+##SBATCH --mem=32000
+
+#SBATCH --mail-type=none
+#SBATCH --time=00:20:59
+
+module load julia/1.4.2
+module load anaconda/3/2019.03 
+
+# each worker can use 1 thread
+export JULIA_NUM_THREADS=6
+
+# we spawn 1 master and N-1 workers from the master
+julia sample-distributed-2.jl
+
